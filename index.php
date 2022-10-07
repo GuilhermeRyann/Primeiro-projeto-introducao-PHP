775598c69a874d912328ac829f5a080a8ed29449
Guilherme Ryann Carvalho Soares <107273990+GuilhermeRyann@users.noreply.github.com>
2022-10-07 15:49:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'Infantil';
+$categorias[] = 'Adolescente';
+$categorias[] = 'Adulto';
+//print_r($categorias);
+
+$nome = 'Eduardo';
+$idade = 18;
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade >= 6 && $idade <=12)
+{
+   for($i =0; $i <= count($categorias); $i++)
+   {
+       if($categorias[$i] == 'Infantil')
+        echo "O nadador ".$nome. " compete na categoria infantil";
+   }
+
+}
+else if($idade >=13 && $idade <=18)
+{
+    for($i =0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'Adolescente')
+         echo "O nadador ".$nome. " compete na categoria adolescente";
+    }
+ 
+}
+else
+{
+    for($i =0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'Adulto')
+         echo "O nadador ".$nome. " compete na categoria adulto";
+    }
+ 
+}
